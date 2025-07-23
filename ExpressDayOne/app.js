dad5157145eda62ae669e8ac5a0467a3fef2392a
Paviterjeet <paviterjeetkaur@gmail.com>
2025-07-23 21:53:22 +0530

    cors, helmet, morgan Middleware (Built-in, Custom, Error handling)

---
+++ ExpressDayOne/app.js
@@ -0,0 +1,38 @@
+import e from "express";
+
+const app = e();
+
+// Built-in middleware
+app.use(e.json());
+// Use Tools (Middleware)
+app.use(cors());            // 1. Allow cross-origin requests
+app.use(helmet());          // 2. Secure your app with headers
+app.use(morgan('dev'));     // 3. Log HTTP requests
+
+
+// Custom middleware
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.url}`);
+  next(); // move to next middleware/route
+}
+// Route that causes an error
+app.get('/error', (req, res, next) => {
+  next(new Error('Something went wrong!'));
+});
+
+// Error handling middleware (must have 4 params)
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).send('Internal Server Error!');
+});
+app.use(logger);
+
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Express.js Recap </h1>")
+})
+
+
+app.listen("3000",()=>{
+    console.log(`http://localhost:3000`)
+})
