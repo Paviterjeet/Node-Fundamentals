00ba9dd349258767f1fc8bc5d2199322d98f6332
Paviterjeet <paviterjeetkaur@gmail.com>
2025-07-23 14:02:31 +0530

    middleware

--- express/app.js
+++ express/app.js
@@ -2,10 +2,31 @@ import e from "express";
 
 const app = e();
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Hello World</h1>")
-})
+app.get(
+  "/",
+  (req, res, next) => {
+    res.write(" 1 ");
+    next();
+  },
+  (req, res, next) => {
+    try {
+        
+      throw new Error(" Gilli gilli");
+      
+    } catch (error) {
+      console.log("This is a catch Box");
+      next(error);
+    }
+  },
+  (req, res, next) => {
+    res.end(" 4 ");
 
-app.listen("3000",()=>{
-    console.log(`http://localhost:3000`)
-})
+  },
+  (err, req, res,next) => {
+    console.log({ "error": err.message });
+  }
+);
+
+app.listen("3000", () => {
+  console.log(`http://localhost:3000`);
+});
