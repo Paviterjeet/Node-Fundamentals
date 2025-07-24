1b2a9f8d6b84cb563c1273d09d66e45dbe3633e9
Paviterjeet <paviterjeetkaur@gmail.com>
2025-07-24 17:55:28 +0530

    DB connect created

--- Mongoose_Mongodb/app.js
+++ Mongoose_Mongodb/app.js
@@ -1,11 +1,22 @@
 import e from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = e();
+dotenv.config()
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Database Connected");
+}).catch(err => console.error(err))
+
+
+
 
 app.get('/',(req, res)=>{
     res.send("<h1> its Working</h1>")
 })
 
-app.listen("3000",()=>{
-    console.log(`http://localhost:3000`)
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`)
 })
