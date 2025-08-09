58d11c3286bb856ad9b70bfccb72478fa7a482e5
Paviterjeet <paviterjeetkaur@gmail.com>
2025-08-09 23:35:47 +0530

    Made Store and Provider and wrapped <App>

--- learnRedux/src/app/store.js
+++ learnRedux/src/app/store.js
@@ -1,1 +1,11 @@
-import counterSlice from 'store'
+import counterReducer from '../features/counterSlice.js'
+import { configureStore } from '@reduxjs/toolkit'
+
+
+export const store = configureStore({
+    reducer : {
+        counter : counterReducer
+    }
+})
+
+
